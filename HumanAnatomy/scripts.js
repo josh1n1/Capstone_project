6d8d544eca0fee4539807ee4d4f604d89defde62
Jerdon07 <jdlitaoen@gmail.com>
2025-01-05 20:26:36 +0800

    Added functions to the buttons

--- HumanAnatomy/scripts.js
+++ HumanAnatomy/scripts.js
@@ -1,3 +1,13 @@
 import * as THREE from 'three';
 
 const canvas = document.getElementById
+
+const partsContainer = document.getElementById("parts-container");
+const parts = Array.from (partsContainer.children);
+
+parts.forEach((child) => {
+    child.addEventListener("click", () => {
+        parts.forEach((part) => (part.style.backgroundColor = "#020234"));
+        child.style.backgroundColor = "#FF7900";
+      });   
+  });
